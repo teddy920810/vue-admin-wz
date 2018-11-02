90e156ded98b3ad63e36c590e3cef396fb70d5b1
teddy920810 <shiy0810@163.com>
2018-11-02 17:24:53 +0800

    配置修改

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: '"production"',
   //BASE_API: '" https://www.easy-mock.com/mock/5b680be31519da4ff895eb54/easy-mock-zw"',
   BASE_API: '"/zx02frmrbwz_pkdza"',
-  IMG_API: '"/"'
+  IMG_API: '""'
 }
